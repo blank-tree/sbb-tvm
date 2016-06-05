5fa64407dcd146b418dd779b31a0cd404bdc50c5
Fernando Obieta <fernando.obieta@blanktree.ch>
2016-06-05 17:32:01 +0200

    keyboard and basic input

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -4,9 +4,9 @@
 	// Custom module for back animations
 	// Puts the class "back" on the ui-view with the class "grid-content"
 	// for animations with css.
-	var app = angular.module('custom', ['ngAnimate']);
+	var viewApp = angular.module('viewController', ['ngAnimate']);
 
-	app.controller('viewCtrl', function ($scope) {
+	viewApp.controller('viewCtrl', function ($scope) {
 
 			var lastScreen = '/';
 
@@ -26,6 +26,76 @@
 
 		});
 
+	var app = angular.module('logic', ['ngAnimate']);
+
+	app.controller('logicCtrl', function ($scope) {
+
+		this.location = 'Zürich HB';
+		this.frequent = ['Bern', 'Basel SBB', 'St. Gallen', 'Genf'];
+
+
+		this.language = 0; // 0 = german; 1 = english; 2 = french; 3 = italian
+
+		this.mainFrom = '';
+		this.mainTo = '';
+		this.mainTime = '';
+		this.mainRoute = '';
+		this.mainType = '';
+		this.mainClass = false; // false = 2. Klasse; true = 1. Klasse
+		this.mainAmount = {
+			'full': 0,
+			'half': 0,
+			'dog': 0,
+			'bike': 0
+		};
+
+	});
+
+	var keyboard = angular.module('keyboard', ['ngAnimate', 'viewController']);
+
+	keyboard.directive('keyboard', function () {
+
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/parts/keyboard.html',
+			controller: function($scope) {
+				var keyboardOutput = $scope['current'];
+				var $targetInput = keyboardOutput == 'search-to' ? $('div.to.input-field span.input-to') : $('div.from.input-field span.input-from');
+				var $keys = $('keyboard div a.sbb-button-grey-small:not(.keyboard-space)');
+
+				$keys.click(function() {
+					$targetInput.append($(this).text());
+				});
+
+				$('keyboard div a.keyboard-space').click(function() {
+					$targetInput.append(' ');
+				});
+
+				$('keyboard div a.keyboard-delete').click(function() {
+					$targetInput.text($targetInput.text().slice(0,-1));
+				});
+
+				this.fillInput = function () {
+
+					if($scope['current'] == 'search-to') {
+						logic.mainTo = $targetInput.text();
+						console.log(logic.MainTo);
+					} else {
+						logic.mainFrom = $targetInput.text();
+						console.log(logic.MainFrom);
+					}
+
+				};
+				
+			},
+			controllerAs: 'keyboard'
+		};
+
+
+	});
+
+
+
 	// Foundation Framework default
 	angular.module('application', [
 		'ui.router',
@@ -37,7 +107,9 @@
 		'foundation.dynamicRouting.animations',
 
 		//custom js
-		'custom'
+		'viewController',
+		'logic',
+		'keyboard'
 	])
 		.config(config)
 		.run(run)
